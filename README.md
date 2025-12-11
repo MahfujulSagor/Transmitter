33126cf445ed89f02ceb6609770e6c86060e7bd0
MahfujulSagor <mahfujulsagor@gmail.com>
2025-12-11 18:40:25 +0600

    add two joys

---
+++ README.md
@@ -0,0 +1,5 @@
+# Transmitter codes
+
+## Warning
+- Nano new boot loader uses 115200 upload speed
+- Nano old boot loader uses 57600 upload speed
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,33 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <Servo.h>
 
-RF24 radio(9, 10);   // CE, CSN
+RF24 radio(9, 10);
 const byte address[6] = "00001";
-
-// Structure for 4-channel data
-struct ControlPacket {
-  byte throttle;
-  byte pitch;
-  byte roll;
-  byte yaw;
-};
-ControlPacket packet;
 
 void setup() {
   Serial.begin(9600);
-
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_250KBPS);
   radio.stopListening();
-
   Serial.println("Transmitter ready...");
 }
 
 void loop() {
+  byte throttle = map(analogRead(A0), 0, 1023, 0, 255); // map 0-1023 to 0-255
+  byte pitch = map(analogRead(A1), 0, 1023, 0, 255); // map 0-1023 to 0-255
+  byte roll = map(analogRead(A2), 0, 1023, 0, 255); // map 0-1023 to 0-255
+  radio.write(&throttle, sizeof(throttle));
+  radio.write(&pitch, sizeof(pitch));
+  radio.write(&roll, sizeof(roll));
 
-  // ----- Read analog stick values -----
-  packet.throttle = map(analogRead(A0), 0, 1023, 0, 255);
-  packet.pitch    = map(analogRead(A1), 0, 1023, 0, 255);
-  packet.roll     = map(analogRead(A2), 0, 1023, 0, 255);
-  packet.yaw      = map(analogRead(A3), 0, 1023, 0, 255);
-
-  // ----- Send the full packet -----
-  radio.write(&packet, sizeof(packet));
-
-  // Debug monitor
-  Serial.print("T: "); Serial.print(packet.throttle);
-  Serial.print(" | P: "); Serial.print(packet.pitch);
-  Serial.print(" | R: "); Serial.print(packet.roll);
-  Serial.print(" | Y: "); Serial.println(packet.yaw);
-
+  Serial.print("Throttle: ");
+  Serial.print(throttle);
+  Serial.print("\t|\tPitch: ");
+  Serial.print(pitch);
+  Serial.print("\t|\tRoll: ");
+  Serial.println(roll);
   delay(10);
 }
