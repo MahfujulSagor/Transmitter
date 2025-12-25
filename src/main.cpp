9770e9c71280e09736f7b75ca9425faaf787e1e8
MahfujulSagor <mahfujulsagor@gmail.com>
2025-12-25 23:22:48 +0600

    final_v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
-#include <SPI.h>
 #include <RF24.h>
+#include <SPI.h>
 
 // CE, CSN pins
 RF24 radio(7, 8);
 
 // 5-byte address (must match RX side)
-const byte address[6] = "DRONE";
+const byte address[6] = {'D', 'R', 'O', 'N', 'E'};
 
-struct ControlPacket {
-  uint16_t throttle;
-  uint16_t yaw;
-  uint16_t pitch;
-  uint16_t roll;
+struct __attribute__((packed)) ControlPacket {
+  uint16_t throttle = 0;
+  uint16_t yaw = 0;
+  uint16_t pitch = 0;
+  uint16_t roll = 0;
 };
+ControlPacket rxData;
 
 void setup() {
   Serial.begin(9600);
@@ -22,8 +23,10 @@ void setup() {
   radio.setChannel(100); // same channel as RX
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_HIGH);
+  radio.setAddressWidth(5);
 
   radio.setAutoAck(true);
+  radio.setRetries(5, 15);
   radio.setCRCLength(RF24_CRC_16);
 
   radio.openWritingPipe(address);
@@ -33,25 +36,25 @@ void setup() {
 }
 
 void loop() {
-  ControlPacket packet;
   // A0 is Throttle, A1 is Yaw, A2 is roll, A3 is Pitch
-  packet.throttle = analogRead(A0); // 0-1023
-  packet.yaw      = analogRead(A1);
-  packet.pitch    = analogRead(A2);
-  packet.roll     = analogRead(A3);
+  rxData.throttle = analogRead(A0); // 0-1023
+  rxData.yaw = analogRead(A1);
+  rxData.pitch = analogRead(A2);
+  rxData.roll = analogRead(A3);
 
-  bool ok = radio.write(&packet, sizeof(packet));
+  bool ok = radio.write(&rxData, sizeof(rxData));
 
-  Serial.print("Throttle: ");
-  Serial.print(packet.throttle);
-  Serial.print("| Yaw: ");
-  Serial.print(packet.yaw);
-  Serial.print("| Pitch: ");
-  Serial.print(packet.pitch);
-  Serial.print("| Roll: ");
-  Serial.println(packet.roll);
+  // Serial.print("T: ");
+  // Serial.print(rxData.throttle);
+  // Serial.print(" | Y: ");
+  // Serial.print(rxData.yaw);
+  // Serial.print(" | P: ");
+  // Serial.print(rxData.pitch);
+  // Serial.print(" | R: ");
+  // Serial.print(rxData.roll);
 
-  Serial.print("ACK: ");
+  // Print whether the packet was acknowledged
+  Serial.print(" | ACK: ");
   Serial.println(ok ? "Yes" : "No");
 
   delay(20);
